7008ad87fbaa1469173e4358a4ef2304b3e9afc0
Scott C Sosna <scott.sosna@budhhadata.com>
2019-04-08 19:31:55 -0500

    module to simplify calling random service

---
+++ random-client/src/main/java/com/buddhadata/projects/jukebox/random/client/RandomHelper.java
@@ -0,0 +1,56 @@
+package com.buddhadata.projects.jukebox.random.client;
+
+import com.buddhadata.projects.jukebox.random.client.services.RandomService;
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+import javax.xml.bind.JAXBContext;
+import java.io.IOException;
+
+/**
+ * Simplify working with random number generator.
+ */
+public enum RandomHelper {
+
+  instance;
+
+  /**
+   * Same JAXB context can be used for all service interfaces created.
+   */
+  private JAXBContext context;
+
+
+  /**
+   * Create Retrofit servces
+   * @param hostNameAndPort the host name to which we are connecting with a port, e.g. myhost:8080
+   * @param type the service interface being instantiated
+   * @return (hopefully) an instantiated service on which we can make calls.
+   */
+  public <T extends Object> T createService (String hostNameAndPort,
+                                             Class<T> type) {
+
+    //  Create the retrofit builder specific to the host.  Could cache, but doubt it
+    //  really will help much
+    Retrofit retrofit = new Retrofit.Builder()
+      .baseUrl("http://" + hostNameAndPort + "/random/")
+      .addConverterFactory(ScalarsConverterFactory.create())
+      .build();
+
+    //  Return the creted service
+    return retrofit.create(type);
+  }
+
+  /**
+   * Quick test method, just substitute your hostname and port.
+   * @param args
+   */
+  public final static void main (String[] args) {
+    RandomService r = RandomHelper.instance.createService ("10.10.9.27:8000", RandomService.class);
+    try {
+      System.out.println(r.getIntegerBound(100).execute().body());
+      System.out.println(r.getBoolean().execute().body());
+    } catch (IOException ioe) {
+      System.out.println (ioe);
+    }
+  }
+}
---
+++ random-client/src/main/java/com/buddhadata/projects/jukebox/random/client/services/RandomService.java
@@ -0,0 +1,54 @@
+package com.buddhadata.projects.jukebox.random.client.services;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Interface defining the functionality exposed by the Random Service
+ */
+public interface RandomService {
+
+  /**
+   * getter
+   * @return boolean from random service
+   */
+  @GET("boolean")
+  Call<Boolean> getBoolean();
+
+  /**
+   * getter
+   * @return double from random service
+   */
+  @GET("double")
+  Call<Double> getDouble();
+
+  /**
+   * getter
+   * @return gaussian from random service
+   */
+  @GET("gaussian")
+  Call<Double> getGaussian();
+
+  /**
+   * getter
+   * @return integer from random service
+   */
+  @GET("int")
+  Call<Integer> getInteger();
+
+  /**
+   * getter
+   * @param bound limit of integer to be returned
+   * @return bound integer from random service
+   */
+  @GET("int/{bound}")
+  Call<Integer> getIntegerBound(@Path("bound") int bound);
+
+  /**
+   * getter
+   * @return long from random service
+   */
+  @GET("long")
+  Call<Long> getLong();
+}
